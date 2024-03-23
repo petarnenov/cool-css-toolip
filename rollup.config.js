dba30d82215de508c52329ce8222f4744e207c4e
Petar Nenov <petarnenovpetrov@gmail.com>
2024-03-23 18:51:49 +0200

    fix: style

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,13 @@
 import babel from '@rollup/plugin-babel';
 import resolve from '@rollup/plugin-node-resolve';
 import external from 'rollup-plugin-peer-deps-external';
-import sass from 'rollup-plugin-sass';
 import postcss from 'rollup-plugin-postcss';
 import image from '@rollup/plugin-image';
 import commonjs from '@rollup/plugin-commonjs';
-import {terser} from 'rollup-plugin-terser';
 import json from '@rollup/plugin-json';
+import {terser} from 'rollup-plugin-terser';
 
+//TODO: add versioning to the output file
 import pkg from './package.json';
 
 export default [
@@ -41,8 +41,8 @@ export default [
                 inject: true,
                 extensions: ['.scss', '.css'],
                 use: ['sass'],
-            })
-            //terser(),
+            }),
+            terser(),
         ],
     },
 ];
