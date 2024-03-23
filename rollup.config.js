f98f616b7ab5d031d472c9ffed26a62b76ad3c84
Petar Nenov <petarnenovpetrov@gmail.com>
2024-03-23 16:16:30 +0200

    add: rollup terser plugin

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,7 @@ import external from 'rollup-plugin-peer-deps-external';
 import sass from 'rollup-plugin-sass';
 import image from '@rollup/plugin-image';
 import commonjs from '@rollup/plugin-commonjs';
+import { terser } from 'rollup-plugin-terser';
 import json from '@rollup/plugin-json';
 
 import pkg from './package.json';
@@ -34,7 +35,8 @@ export default [
             sass(),
             image(),
             commonjs(),
-            json()
+            json(),
+            terser(),
         ],
     },
 ];
