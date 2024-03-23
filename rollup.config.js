5942e336d982cd17abdb724ef5a15156a1a4b9d3
Petar Nenov <petarnenovpetrov@gmail.com>
2024-03-23 17:35:38 +0200

    add: css file to dist

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ import external from 'rollup-plugin-peer-deps-external';
 import sass from 'rollup-plugin-sass';
 import image from '@rollup/plugin-image';
 import commonjs from '@rollup/plugin-commonjs';
-import { terser } from 'rollup-plugin-terser';
+import {terser} from 'rollup-plugin-terser';
 import json from '@rollup/plugin-json';
 
 import pkg from './package.json';
@@ -32,7 +32,9 @@ export default [
             }),
             external(),
             resolve(),
-            sass(),
+            sass({
+                output: 'dist/index.css',
+            }),
             image(),
             commonjs(),
             json(),
